aa9cd3a0a29b56e79bec54b8107665d884ba0350
Rodrigo Gaspar <rodrigogaspar@MacBook-Air-de-Rodrigo.local>
2021-03-25 18:18:25 -0300

    First commit

---
+++ SimpleMovieApp/Controllers/MovieVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleMovieApp
+//
+//  Created by Rodrigo Gaspar on 25/03/21.
+//
+
+import UIKit
+
+class MovieVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+
+
+}
+
---
+++ SimpleMovieApp/Views/MovieView.swift
@@ -0,0 +1,8 @@
+//
+//  MovieView.swift
+//  SimpleMovieApp
+//
+//  Created by Rodrigo Gaspar on 25/03/21.
+//
+
+import Foundation
