699ac777875ed4a79e5f19a863e8af41cc17a482
Rodrigo Gaspar <rodrigogaspar@MacBook-Air-de-Rodrigo.local>
2021-03-25 20:40:13 -0300

    Movie model structed and DAO

---
+++ SimpleMovieApp/Models/DAO/DAOMovie.swift
@@ -0,0 +1,85 @@
+//
+//  DAOMovie.swift
+//  SimpleMovieApp
+//
+//  Created by Rodrigo Gaspar on 25/03/21.
+//
+
+import Foundation
+import UIKit
+
+class DAOMovie {
+    
+    fileprivate init() {
+        
+    }
+    
+    static func transformJSONInArrayMovie(_ JSON : AnyObject) -> [Movie] {
+        
+        var array : [Movie] = []
+        
+        guard let data = JSON as? NSArray else {
+            return array
+        }
+        
+        for item in data {
+            
+            array.append(transformJSONInMovie(item as AnyObject))
+            
+        }
+        
+        return array
+        
+    }
+    
+    static func transformJSONInMovie(_ JSON : AnyObject) -> Movie {
+        
+        let item = Movie()
+        
+        if let info = JSON["title"] as? String {
+            item.title = info
+        }
+        
+        if let info = JSON["vote_count"] as? Int {
+            item.voteCount = info
+        }
+        
+        if let info = JSON["popularity"] as? Int {
+            item.popularity = info
+        }
+        
+        if let info = JSON["release_date"] as? String {
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            dateFormatter.timeZone = TimeZone.current
+            
+            if let date = dateFormatter.date(from: info) {
+                
+                item.releaseDate = date
+                
+            }
+            
+        }
+        
+        if let genres = JSON["genres"] as? [[String : Any]] {
+            
+            for genre in genres {
+                
+                if let name = genre["name"] as? String {
+                    
+                    item.genres.append(name)
+                    
+                }
+                
+            }
+            
+        }
+
+        return item
+        
+    }
+    
+}
+
+
---
+++ SimpleMovieApp/Models/Models-Class/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  SimpleMovieApp
+//
+//  Created by Rodrigo Gaspar on 25/03/21.
+//
+
+
+import UIKit
+
+class Movie {
+    
+    var title : String = ""
+    var voteCount : Int = -1
+    var popularity : Int = -1
+    var releaseDate : Date!
+    var genres : [String] = []
+    
+}
+
